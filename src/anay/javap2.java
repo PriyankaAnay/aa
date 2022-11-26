ea254c005bb8b1a69da9cc935650c934fc23ff75
Admin <Admin@Admin-PC>
2022-11-26 15:00:15 +0530

    asss

---
+++ src/anay/javap2.java
@@ -0,0 +1,27 @@
+package anay;
+
+public class javap2 {
+
+	public static void main(String[] args) {
+		int num=6;
+		
+		int count=0;
+		for(int i=2;i<num;i++)
+		{
+			if(num%i==0)
+			{
+				count++;
+				break;
+			}
+			
+		}
+	if(count==1)
+	{
+		System.out.println("not prime");
+	}
+	else
+	{
+		System.out.println("prime");
+	}
+	}
+}
